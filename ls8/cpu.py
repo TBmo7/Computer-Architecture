9573e553aa2e75ba6fda0e07d0f0fdfc099bd25e
Troy Moore <tbmoore85@gmail.com>
2020-10-02 21:33:52 -0400

    Finishing first and second day

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,25 +6,52 @@ class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.reg[7] = 0xF4
+        self.ram = [0] * 256
+        self.pc = 0 #program counter
+        self.ir = 0 #instruction register
+        self.fl = [0] * 8 #Flags
+        self.halted = False
+        
+    def ram_read(self,MAR):
+        #takes in an address, and returns the value stored there
+        return self.ram[MAR]
+        
 
-    def load(self):
+    def ram_write(self,MAR,MDR):
+        #takes in an address MAR and data to write there MDR, then writes data to address
+        self.ram[MAR] = MDR
+
+
+
+
+    def load(self,file):
         """Load a program into memory."""
+        program = []
+        try:
+            with open(file) as f:
+                for line in f:
+                    program.append(line.split('#')[0].split())
+        except:
+            print('file not found')
+            sys.exit(1)            
+
+
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
+        #program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+            #0b10000010, # LDI R0,8
+            #0b00000000,
+            #0b00001000,
+            #0b01000111, # PRN R0
+            #0b00000000,
+            #0b00000001, # HLT
+        #]
 
         for instruction in program:
             self.ram[address] = instruction
@@ -34,9 +61,84 @@ class CPU:
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
-        if op == "ADD":
+        if op == 0b10100000: #ADD
             self.reg[reg_a] += self.reg[reg_b]
+            self.pc += 3
         #elif op == "SUB": etc
+        elif op == "AND":
+            pass
+        elif op == "CALL":
+            pass
+        elif op == "CMP":
+            pass
+        elif op == "DEC":
+            pass
+        
+        elif op == "DIV":
+            pass
+        elif op == 0b00000001: #HLT
+            self.halted = True
+            self.pc += 1
+        elif op == "INC":
+            pass
+        elif op == "INT":
+            pass
+        elif op == "IRET":
+            pass
+        elif op == "JEQ":
+            pass
+        elif op == "JGE":
+            pass
+        elif op == "JGT":
+            pass
+        elif op == "JLE":
+            pass
+        elif op == "JLT":
+            pass
+        elif op == "JMP":
+            pass
+        elif op == "JNE":
+            pass
+        elif op == "LD":
+            pass
+        elif op == 0b10000010: #LDI
+            #sets the value of a register to an integer
+            self.reg[reg_a] = reg_b
+            self.pc +=3
+            
+        elif op == "MOD":
+            pass
+        elif op == 0b10100010 :#MUL
+            self.reg[reg_a] *= self.reg[reg_b]
+            self.pc += 3
+        elif op == "NOP":
+            pass
+        elif op == "NOT":
+            pass
+        elif op == "OR":
+            pass
+        elif op == "POP":
+            pass
+        elif op == "PRA":
+            pass
+        elif op == 0b01000111: #PRN
+            print(self.reg[reg_a])
+            self.pc += 2
+        elif op == "PUSH":
+            pass
+        elif op == "RET":
+            pass
+        elif op == "SHL":
+            pass
+        elif op == "SHR":
+            pass
+        elif op == "ST":
+            pass
+        elif op == 0b10100001: #SUB
+            self.reg[reg_a] -= self.reg[reg_b]
+            self.pc += 2
+        elif op == "XOR":
+            pass
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -62,4 +164,15 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while not self.halted :#HLT#<<This is probably not right
+            self.ir = self.ram_read(self.pc)#copy the register value into the instruction register
+            operand_a = self.ram_read(self.pc+1)
+            operand_b = self.ram_read(self.pc+2)
+            self.alu(self.ir,operand_a,operand_b)
+            #self.pc +=1
+
+curr_file = "ls8/examples/mult.ls8"        
+
+PC1 = CPU()
+PC1.load(curr_file)
+PC1.run()
